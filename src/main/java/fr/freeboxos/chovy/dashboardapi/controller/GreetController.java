e26fee7e5b3ce9685fcb2f2526b67f0adb2aa66a
yann <jeanmaireyann.pro@gmail.com>
2023-07-13 20:21:58 +0200

    test hook

--- src/main/java/fr/freeboxos/chovy/dashboardapi/controller/GreetController.java
+++ src/main/java/fr/freeboxos/chovy/dashboardapi/controller/GreetController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "t une merde";
     }
 }
