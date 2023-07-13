3dd45d614ed89dae53d8ae045877eaef10dc140f
yann <jeanmaireyann.pro@gmail.com>
2023-07-13 20:14:28 +0200

    test hook

---
+++ src/main/java/fr/freeboxos/chovy/dashboardapi/controller/GreetController.java
@@ -0,0 +1,12 @@
+package fr.freeboxos.chovy.dashboardapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
