af9ca62b714f128abb4720defd277fe7df51799a
Jeanmaire dit cartier <yann.jeanmaire@gmail.com>
2023-07-23 01:20:21 +0200

    added toto

---
+++ src/main/java/fr/freeboxos/chovy/dashboardapi/controller/o.java
@@ -0,0 +1,11 @@
+package fr.freeboxos.chovy.dashboardapi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class o {
+
+    public String monitorBd() {
+        return "je suis unecatind";
+    }
+}
