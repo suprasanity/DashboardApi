f8909e2a8d1d3554cfcaf25c8316e6b14e5df9da
Jeanmaire dit cartier <yann.jeanmaire@gmail.com>
2023-07-22 23:52:05 +0200

    test sonar

--- src/main/java/fr/freeboxos/chovy/dashboardapi/controller/MonitoringController.java
+++ src/main/java/fr/freeboxos/chovy/dashboardapi/controller/MonitoringController.java
@@ -3,7 +3,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/monitoring")
 public class MonitoringController {
-//just for testddr
+//just for testddrq
     public String monitorBd(){
         return "true";
     }
