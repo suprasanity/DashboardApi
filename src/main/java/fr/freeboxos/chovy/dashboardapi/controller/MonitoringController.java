4d809356602618da5cf53186f86d4c7466b4bf69
Jeanmaire dit cartier <yann.jeanmaire@gmail.com>
2023-07-23 02:30:29 +0200

    fix

--- src/main/java/fr/freeboxos/chovy/dashboardapi/controller/MonitoringController.java
+++ src/main/java/fr/freeboxos/chovy/dashboardapi/controller/MonitoringController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/hellol")
 public class MonitoringController {
 
-    @GetMapping("/hellpo")
+    @GetMapping("/heldlpo")
     public String monitorBd(){
         return "true";
     }
