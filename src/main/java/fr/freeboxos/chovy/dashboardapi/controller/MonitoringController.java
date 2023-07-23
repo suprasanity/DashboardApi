e10cabf532e60eec486192fdb83f17c7ae469fdf
Jeanmaire dit cartier <yann.jeanmaire@gmail.com>
2023-07-23 02:24:35 +0200

    fix

--- src/main/java/fr/freeboxos/chovy/dashboardapi/controller/MonitoringController.java
+++ src/main/java/fr/freeboxos/chovy/dashboardapi/controller/MonitoringController.java
@@ -2,10 +2,10 @@ package fr.freeboxos.chovy.dashboardapi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/hello")
+@RestController("/hellol")
 public class MonitoringController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hellpo")
     public String monitorBd(){
         return "true";
     }
